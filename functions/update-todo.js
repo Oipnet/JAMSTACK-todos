6d0df2747cb07b80b9af6edbd0710ac7ff5a8c74
Arnaud POINTET <arnaud@oipnet.info>
2020-03-11 20:02:51 +0100

    Feat : complete tadk

---
+++ functions/update-todo.js
@@ -0,0 +1,37 @@
+const sendQuery = require('./utils/send-query');
+const UPDATE_TODO = `
+  mutation($text: String!, $id: ID!, $completed: Boolean){
+    updateTodo(
+      id: $id,
+      data: {
+        text: $text
+        completed: $completed
+      }
+    ) {
+      _id 
+      text 
+      completed
+    }
+  }
+`;
+
+exports.handler = async event => {
+  const { text, id, completed } = JSON.parse(event.body);
+  const { data, errors } = await sendQuery(UPDATE_TODO, {
+    text,
+    id,
+    completed
+  });
+
+  if (errors) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errors)
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ todo: data.updateTodo })
+  };
+};
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
 import styles from './todo.module.css';
 
-const Todo = ({ todo }) => (
-  <p className={`${styles.text} ${todo.completed && styles.completed}`}>
-    {todo.text}
-  </p>
-);
+const Todo = ({ todo, reloadTodos }) => {
+  const handleUpdateTodo = async event => {
+    event.preventDefault();
+
+    await axios.post('/api/update-todo', {
+      id: todo._id,
+      text: todo.text,
+      completed: !todo.completed
+    });
+
+    reloadTodos();
+  };
+  return (
+    <p
+      className={`${styles.text} ${todo.completed && styles.completed}`}
+      onClick={handleUpdateTodo}
+    >
+      {todo.text}
+    </p>
+  );
+};
 
 export default Todo;
